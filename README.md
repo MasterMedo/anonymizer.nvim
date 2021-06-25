934efb18cf85853fe13c1e6e41fcdc119f70b98c
MasterMedo <mislav.vuletic@gmail.com>
2021-06-25 16:06:49 +0200

    initialise anonymizer

---
+++ README.md
@@ -0,0 +1,20 @@
+# neovim buffer anonymizer
+
+1. set up python path in your init.vim
+```vim
+let g:python3_host_prog = '/usr/bin/python3'
+let g:python_host_prog = '/usr/bin/python2'
+```
+2. add plugin
+```vim
+Plug 'mastermedo/anonymizer.vim'
+```
+3. install and update remote plugins
+```vim
+:PlugInstall
+:UpdateRemotePlugins
+```
+4. use anonymizer
+```
+map <leader><leader> :call AnonymizeBuffer()<cr>
+```
---
+++ rplugin/python3/anonymizer.py
@@ -0,0 +1,78 @@
+import pynvim
+
+
+@pynvim.plugin
+class BufferAnonymizer(object):
+    def __init__(self, nvim):
+        self.nvim = nvim
+
+    @pynvim.function("AnonymizeBuffer")
+    def anonymize_buffer(self, _):
+        src = self.nvim.new_highlight_source()
+        top_line = self.nvim.funcs.line("w0")
+        bottom_line = self.nvim.funcs.line("w$")
+        lines = self.nvim.funcs.getline(top_line, bottom_line)
+        for lnum, line in enumerate(lines, top_line):
+            new_line = ' '
+            blobs = self.get_syn_groups(lnum, line)
+            for synid, start, end in blobs:
+                new_line += ' ' * (start - len(new_line) + 1)
+                if start == end:
+                    raise Exception(f'start and end are the same: {start}')
+                if start + 1 == end:
+                    new_line += 'O'
+                else:
+                    new_line += '('
+                    for col in range(start + 1, end - 1):
+                        new_line += 'D'
+                    new_line += ')'
+
+            self.nvim.current.buffer[lnum - 1] = new_line[2:]
+            for synid, start, end in blobs:
+                self.nvim.current.buffer.add_highlight(
+                    self.nvim.funcs.synIDattr(synid, 'name'),
+                    lnum - 1,
+                    start - 1,
+                    end - 1,
+                    src_id=src
+                )
+
+    def get_syn_groups(self, lnum, line):
+        blobs = []
+        started = False
+        for col, char in enumerate(line, 1):
+            if not started:
+                if char.isspace():
+                    continue
+                else:
+                    started = True
+                    start = col
+                    synid = self.nvim.funcs.synID(lnum, col, False)
+
+            new_synid = self.nvim.funcs.synID(lnum, col, 1)
+            if char.isspace():
+                if line[col - 2].isspace():
+                    blobs.append([
+                        synid,
+                        start,
+                        col - 1
+                    ])
+                    started = False
+                continue
+
+            colour = self.nvim.funcs.synIDattr(
+                self.nvim.funcs.synIDtrans(synid), "fg")
+            new_colour = self.nvim.funcs.synIDattr(
+                self.nvim.funcs.synIDtrans(new_synid), "fg")
+            if colour != new_colour:
+                blobs.append([
+                    synid,
+                    start,
+                    col - 1 if line[col - 2].isspace() else col
+                ])
+                synid = new_synid
+                start = col
+
+        if started:
+            blobs.append([synid, start, col + 1])
+        return blobs
